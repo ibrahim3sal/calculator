08f47b9f133e99eebce76bfc0971f58104011d3a
Ibrahim Asal <ibrahim3asal@gmail.com>
2026-09-13 01:37:16 +0300

    feat: initialize calculator project

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
+while True:
+    calc = input("Enter your calculation: ")
 
+    if "+" in calc:
+        float_num1, float_num2 = calc.split("+")
+        result = float(float_num1) + float(float_num2)
+
+    elif "-" in calc:
+        float_num1, float_num2 = calc.split("-")
+        result = float(float_num1) - float(float_num2)
+
+    elif "*" in calc:
+        float_num1, float_num2 = calc.split("*")
+        result = float(float_num1) * float(float_num2)
+
+    elif "/" in calc:
+        float_num1, float_num2 = calc.split("/")
+        result = float(float_num1) / float(float_num2)
+        print("Result:", result)
+
+    another_calc = input("Do you want to perform another calculation? (y/n): ")
+
+    if another_calc.lower() != "y":
+        break
